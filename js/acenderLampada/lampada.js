d235c89ad96ee5b8cf9e777c18763189d9cf5b57
vava20br <vava20br@gmail.com>
2024-04-16 17:53:59 -0300

    primeira_versao

---
+++ js/acenderLampada/lampada.js
@@ -0,0 +1,10 @@
+window.onload = function()
+{
+    var elementoBotao = document.getElementById("elementoBotao");
+    elementoBotao.addEventListener("click", click);
+
+    function click()
+    {
+        
+    }
+}
